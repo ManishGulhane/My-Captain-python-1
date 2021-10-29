eae457df19fa46536aac2abbebcb5307cfd3bd41
ManishGulhane <90751777+ManishGulhane@users.noreply.github.com>
2021-10-29 22:21:26 +0530

    Update and rename README.md to Area of circle.py

---
+++ Area of circle.py
@@ -0,0 +1,6 @@
+# Area-of-circle
+radius = float(input("Input the radius of circle : "))
+area = 3.14*radius*radius
+display="The area of the circle with radius {} is : {}"
+A1=display.format(radius, area)
+print(A1)
